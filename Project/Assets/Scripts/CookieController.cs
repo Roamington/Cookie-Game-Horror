34661e8cf6cd0593bcb6ed1da5a029c56887473c
Pokerface777 <lucasbeil98@yahoo.de>
2015-10-18 21:39:18 +0200

    Cookie Game Horror
    Cookie game with horror moment.

---
+++ Project/Assets/Scripts/CookieController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CookieController : MonoBehaviour {
+
+    public GameObject WinPanel;
+    public GameObject LosePanel;
+    public static int Cookies;
+
+    void Start ()
+    {
+        Cookies = 0;
+    }
+
+	public void Controller (int ButtonID)
+    {
+        if (ButtonID == 0)
+        {
+            PublicView.CookieCount++;
+            Cookies++;
+
+            PublicView.GameStarted = true;
+        }
+
+        if (ButtonID == 1)
+        {
+            LosePanel.GetComponent<CanvasGroup>().alpha = 0;
+            LosePanel.GetComponent<CanvasGroup>().interactable = false;
+            LosePanel.GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+            Cookies = 0;
+            PublicView.RestartGame = true;
+        }
+
+        if (ButtonID == 2)
+        {
+            WinPanel.GetComponent<CanvasGroup>().alpha = 0;
+            WinPanel.GetComponent<CanvasGroup>().interactable = false;
+            WinPanel.GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+            Cookies = 0;
+            PublicView.RestartGame = true;
+        }
+
+    }
+}
---
+++ Project/Assets/Scripts/PublicView.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PublicView : MonoBehaviour {
+    
+    public static int CookieCount;
+    public Text CookieCountUI;
+    public Text CookieTimerUI;
+    public Text NeedCookiesUI;
+
+    public float CookieTimer;
+    public int CookieTime;
+
+    public static bool GameStarted;
+    public static bool RestartGame;
+
+    public GameObject WinPanel;
+    public GameObject LosePanel;
+
+    // Use this for initialization
+	void Start () {
+
+        CookieCount = 0;
+        CookieTime = 25;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (RestartGame == true)
+        {
+            CookieCount = 0;
+            CookieTime = 25;
+
+            GameStarted = false;
+            RestartGame = false;
+        }
+
+        NeedCookiesUI.text = "" + CookieController.Cookies + " of 125";
+
+        if (GameStarted == true)
+        {
+
+            CookieTimer += Time.deltaTime;
+
+            if (CookieTimer >= 1f)
+            {
+
+                CookieTime--;
+                CookieTimer = 0;
+            }
+        }
+
+        CookieCountUI.text = CookieCount.ToString();
+        CookieTimerUI.text = CookieTime.ToString();
+	    
+        if (CookieCount >= 125 && CookieTime >= 0)
+        {
+            // Win
+            Application.OpenURL("https://akk.li/pics/anne/jpg");
+            WinPanel.GetComponent<CanvasGroup>().alpha = 1;
+            WinPanel.GetComponent<CanvasGroup>().interactable = true;
+            WinPanel.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        }
+
+        if (CookieCount != 125 && CookieTime <= 0)
+        {
+
+            LosePanel.GetComponent<CanvasGroup>().alpha = 1;
+            LosePanel.GetComponent<CanvasGroup>().interactable = true;
+            LosePanel.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        }
+
+	}
+}
